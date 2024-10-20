b77b17b7984bb7aa5738e42b7a8c885b66733722
HP <HP@LAPTOP-U922A9UO>
2024-10-20 15:22:29 +0200

    rechnungsNummer, kunde, artikelListe, rechungsDatum

--- src/accounting/Rechnung.java
+++ src/accounting/Rechnung.java
@@ -1,5 +1,14 @@
 package accounting;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Rechnung {
 
+	 private static final AtomicInteger idCounter = new AtomicInteger(0);
+	    private final int rechnungsNummer;
+	    private Kunde kunde;
+	    private List<Artikel> artikelListe;
+	    private Date rechnungsDatum;
+
+	
 }
