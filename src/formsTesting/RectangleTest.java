e5b4276c71a62f637362fa9319b57e67ebfb4900
HP <HP@LAPTOP-U922A9UO>
2024-10-21 17:41:04 +0200

    tested Method testToString

--- src/formsTesting/RectangleTest.java
+++ src/formsTesting/RectangleTest.java
@@ -10,4 +10,24 @@ public class RectangleTest {
 		  double area = r.calculateArea();
 		  Assert.assertEquals(area, 5*4);
 	  }
+
+		
+@Test
+public void testCalculateCircumference() {
+	  Rectangle r = new Rectangle(7, 9, 2, 6);
+	  double circumference = r.calculateCircumference();
+	  Assert.assertEquals(circumference, 2 + 6);
+}
+
+
+
+@Test
+public void testToString() {
+	  Rectangle r = new Rectangle(1, 2, 3, 4);
+	  String text = r.toString();
+	  Assert.assertEquals(text, "Rectangle (" + r.getX() + "|" + r.getY() + ") with width = " + r.getWidth() + " and length = " + r.getLength());
+}
+
+
+
 }
