cd039d925c8a439617980a6211a9f8028f31c40a
HP <HP@LAPTOP-U922A9UO>
2024-10-22 08:47:06 +0200

    Testing Methods: CalculateArea, CalculateCircumference, ToString

--- src/formsTesting/SquareTest.java
+++ src/formsTesting/SquareTest.java
@@ -1,9 +1,32 @@
 package formsTesting;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTest {
-  @Test
-  public void f() {
-  }
+
+      @Test
+      public void testCalculateArea() {
+    	  Square ss = new Square (1, 2, 3);
+    	  double area = ss.calculateArea();
+    	  Assert.assertEquals(area, 3*1);
+      }
+    
+    
+
+      @Test
+      public void testCalculateCircumference() {
+    	  Square ss = new Square(3, 2, 1);
+    	  double circumference = ss.calculateCircumference();
+    	  Assert.assertEquals(circumference, (1+1)*2);
+      }
+      
+      
+      @Test
+      public void testToString() {
+    	  Square ss = new Square (3,3,3);
+    	  String text = ss.toString();
+    	  Assert.assertEquals(text, "Square (" + ss.getX() + "|" + ss.getY() + ") with width = " + 3.0);
+      }
+      
 }
